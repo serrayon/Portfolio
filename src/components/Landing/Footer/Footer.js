2310b04408b9efe1c6153485cce17fbbf2c35b5f
serrayon <49975993+serrayon@users.noreply.github.com>
2022-11-19 00:25:16 -0800

    updated linkedin
    updated linkedin

--- src/components/Landing/Footer/Footer.js
+++ src/components/Landing/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
           <div className="social-links">
             {/* <a href=""><i className="fa fa-facebook-square" aria-hidden="true"></i></a> */}
             <a href="https://github.com/serrayon"><i className="fa fa-github" aria-hidden="true"></i></a>
-            <a href="https://www.linkedin.com/in/octavio-serrano-8b964b183/"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
+            <a href="https://https://www.linkedin.com/in/octavio-serrano/"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
           </div>
           {/* <input type="text" placeholder="Email Address" />
           <button type="submit">Subscribe</button> */}
