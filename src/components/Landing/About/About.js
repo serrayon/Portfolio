298d6370ab6e1d29213a086402af788ba5e8bd24
Serrayon <serrayon81@gmail.com>
2023-12-05 16:49:11 -0800

    update about and resume

--- src/components/Landing/About/About.js
+++ src/components/Landing/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <section id="about" className="callout-about">
         <article className="wrap">
           <h2>On a new path...</h2>
-          <p className='aboutp'>As a Quality Engineer/Analyst, my natural curiosity and desire to constantly learn and improve have led me on an unconventional learning path. I have always been drawn to understanding how things work and seeking out new knowledge and skills to further my understanding in my field. This drive has allowed me to take on a diverse range of projects and challenges, and has helped me to develop a strong foundation in quality engineering and analysis. I am excited to continue my journey as a Quality Engineer/Analyst and to use my skills and knowledge to make a positive impact in the field.</p>
+          <p className='aboutp'>As a Quality Engineer/Analyst, my natural curiosity and desire to constantly learn and improve have led me on an unconventional learning path. I have always been drawn to understanding how things work and seeking out new knowledge and skills to further my understanding in my field. This drive has allowed me to take on a diverse range of projects and challenges, helping me develop a strong foundation in quality engineering and analysis. I am excited to continue my journey as a Quality Engineer/Analyst and to use my skills and knowledge to make a positive impact in the field.</p>
           <p> </p>
           <p></p>
         </article>
--- src/components/Resume/Main/Main.js
+++ src/components/Resume/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Picture1 from './Pictures/Octavio_Serrano_QA.jpg';
+import Picture1 from './Pictures/Octavio_Serrano_QA-jpeg.jpg';
+
 //import Picture2 from './Pictures/0002.jpg';
 // import Picture9 from './Pictures/about.jpg';
 import './Main.css';
