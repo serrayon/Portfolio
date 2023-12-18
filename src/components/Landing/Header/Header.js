d2b6a4485a361cbbed54394f89132501ce474eb5
Serrayon <serrayon81@gmail.com>
2023-12-18 13:07:17 -0800

    sunset

--- src/components/Landing/Header/Header.js
+++ src/components/Landing/Header/Header.js
@@ -8,6 +8,7 @@ const Header = () => {
       <header className="vertical-center header-landing">
         <div>
           <h1><a href="/">Octavio Serrano</a></h1>
+          <img src={sunset} alt="Sunset" />
           <h2></h2>
         </div>
       </header>
