affa677f8c6d69328d54e828e003733af51f90c1
serrayon <serrayon81@gmail.com>
2024-01-04 20:17:05 -0800

    about me to QA

--- src/components/Landing/About/About.js
+++ src/components/Landing/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <section id="about" className="callout-about">
         <article className="wrap">
           <h2>On a new path...</h2>
-          <p className='aboutp'>I have always been interested in web development, and recently completed the 3 month Software Engineering Immersive program at General Assembly in San Francisco. In the last 3 months I have become familiar with full stack technologies. I am now on the market and actively seeking a junior web development position.</p>
+          <p className='aboutp'>I am embarking on an exciting career shift to Software Quality Assurance, leveraging my diverse skill set and passion for technology and linguistics. I have a solid foundation in programming, and Quality Assurance. My experience was gained through the successful completion of a  Software Engineering, and Manual Quality Assurance bootcamps. My proficiency in executing comprehensive testing strategies is complemented by a keen understanding of industry-standard QA tools, including Testrail and Jira. Beyond technical expertise, I pride myself on possessing strong communication and interpersonal skills, both in verbal and written forms. This enables me to articulate complex testing concepts, collaborate seamlessly with diverse teams, and contribute effectively to the continuous improvement of software quality.</p>
           <p> </p>
           <p></p>
         </article>
